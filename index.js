5e6f007f46622611bdde3ab462cdeeac2bcdad3c
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-10 10:28:47 +0100

    added schema to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const db = require("./db");
+const Image = require("./image/model");
 
 app.listen(port, () => {
   console.log("Listening on port: ", port);
