a914cb2eadfe81f75fcbce60ad6449f978e8a297
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-10 10:09:25 +0100

    defined db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
+const db = require("./db");
 
 app.listen(port, () => {
   console.log("Listening on port: ", port);
