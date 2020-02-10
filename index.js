d80353f4fa1724ac523925a92e634523bfd8beed
Shashank Neelagiri <sneelagiri@outlook.com>
2020-02-10 10:38:10 +0100

    added post method, cors, and body parser

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 4000;
 const db = require("./db");
 const Image = require("./image/model");
 const imageRouter = require("./image/router");
-
+const cors = require("cors");
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+app.use(express.json());
 app.use(imageRouter);
 
 app.listen(port, () => {
